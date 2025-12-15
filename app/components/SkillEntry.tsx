7cd8a22efc050bd040aa25608a2b98d6bea8c9c2
the_kd2crazy <23230@ecam.be>
2025-12-15 13:11:59 +0100

    PAGE compétences réaliser

--- app/components/SkillEntry.tsx
+++ app/components/SkillEntry.tsx
@@ -8,7 +8,7 @@ type SkillEntry = {
 
 export default function SkillEntry(props: SkillEntry) {
   return (
-    <div className="bg-[#3F4041] text-slate-50 flex flex-col items-center justify-center w-30 h-30 lg:w-36 lg:h-36 rounded-3xl hover:bg-[#E7BF76] transition-all duration-100">
+    <div className="bg-orange-500 text-slate-50 flex flex-col items-center justify-center w-30 h-30 lg:w-36 lg:h-36 rounded-3xl hover:bg-orange-300 transition-all duration-100">
       {props.children}
       <p className="text-lg text-center">{props.title}</p>
     </div>
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,10 +1,20 @@
-
+import SkillEntry from "../components/SkillEntry";
+import { SiJavascript, SiTypescript, SiCss3, SiHtml5, SiPython, SiSharp, SiCplusplus, SiArduino } from 'react-icons/si';
 
 export default function SKILLS() {
     return (
-
-        <h1>
-            Mes SKILLS
-        </h1>
+        <>
+            <h1 className="text-3xl text-white font-bold text-center pb-12 mt-2">MON BAGAGE TECHNIQUE</h1>
+            <section className="mx-25 w-full max-w-7xl px-6 py-9  items-center grid gap-6 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-3 xl:grid-cols-4">
+                <SkillEntry title="Python"><SiPython size={50} /> </SkillEntry>
+                <SkillEntry title="C#"><SiSharp size={50} /> </SkillEntry>
+                <SkillEntry title="Javascript"><SiJavascript size={50} /> </SkillEntry>
+                <SkillEntry title="Html"><SiHtml5 size={50} /> </SkillEntry>
+                <SkillEntry title="C++"><SiCplusplus size={50} /> </SkillEntry>
+                <SkillEntry title="CSS"><SiCss3 size={50} /> </SkillEntry>
+                <SkillEntry title="Typscript"><SiTypescript size={50} /> </SkillEntry>
+                <SkillEntry title="Arduino"><SiArduino size={50} /> </SkillEntry>
+            </section>
+        </>
     )
 }
